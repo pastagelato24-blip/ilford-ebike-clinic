b1d911b9118dfbd1d3e53c52aac9c97444145a5e
pastagelato24-blip <pastagelato24@gmail.com>
2025-11-08 19:59:20 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Ilford E-Bike Clinic</h1>
+      <p>Repairs for e-bikes, e-scooters & electric mopeds.</p>
+      <p>WhatsApp: 07510 935262</p>
+    </div>
+  );
+}
